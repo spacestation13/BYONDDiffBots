2fd8481dff07c39272074dc22a484d46edc6704f
Katherine Kiefer <sterlite2902@gmail.com>
2022-10-24 09:02:02 +1100

    loop indefinitely, timeout

--- crates/icondiffbot2/src/runner.rs
+++ crates/icondiffbot2/src/runner.rs
@@ -1,16 +1,20 @@
+use std::time::Duration;
+
 use super::job_processor::do_job;
 use diffbot_lib::job::types::Job;
 
 pub async fn handle_jobs<S: AsRef<str>>(name: S, mut job_receiver: yaque::Receiver) {
-    while let Ok(jobguard) = job_receiver.recv().await {
-        let job = rmp_serde::from_slice(&jobguard);
-        match job {
-            Ok(job) => job_handler(name.as_ref(), job).await,
-            Err(err) => eprintln!("{}", err),
+    loop {
+        if let Ok(jobguard) = job_receiver.recv().await {
+            let job = rmp_serde::from_slice(&jobguard);
+            match job {
+                Ok(job) => job_handler(name.as_ref(), job).await,
+                Err(err) => eprintln!("{}", err),
+            }
+            if let Err(err) = jobguard.commit() {
+                eprintln!("{}", err)
+            };
         }
-        if let Err(err) = jobguard.commit() {
-            eprintln!("{}", err)
-        };
     }
 }
 
@@ -26,7 +30,11 @@ async fn job_handler(name: &str, job: Job) {
 
     let _ = check_run.mark_started().await;
 
-    let output = actix_web::rt::task::spawn_blocking(move || do_job(job)).await;
+    let output = actix_web::rt::time::timeout(
+        Duration::from_secs(3600),
+        actix_web::rt::task::spawn_blocking(move || do_job(job)),
+    )
+    .await;
 
     println!(
         "[{}#{}] [{}] Finished",
@@ -34,6 +42,15 @@ async fn job_handler(name: &str, job: Job) {
         pull_request,
         check_run.id()
     );
+
+    let output = {
+        if let Err(_) = output {
+            eprintln!("Job timed out!");
+            let _ = check_run.mark_failed("Job timed out after 1 hours!").await;
+            return;
+        }
+        output.unwrap()
+    };
 
     if let Err(e) = output {
         let fuckup = match e.try_into_panic() {
