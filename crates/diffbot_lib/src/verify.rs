ae05e028d93faa64ece17049af7ff6918789380f
Katherine Kiefer <sterlite2902@gmail.com>
2023-01-15 19:37:55 +1100

    fix verify, add some tracing

--- crates/diffbot_lib/src/verify.rs
+++ crates/diffbot_lib/src/verify.rs
@@ -1,4 +1,4 @@
-use hmac::{Hmac, Mac};
+use hmac::{digest::CtOutput, Hmac, Mac};
 use sha2::Sha256;
 
 type HmacSha256 = Hmac<Sha256>;
@@ -13,12 +13,24 @@ pub fn verify_signature(
             return Err(actix_web::error::ErrorBadRequest("Expected signature in header"))
         };
 
+        log::trace!("Signature received: {}", sig.clone());
+
         //remove the `sha256=` part
         let (_, actual_signature) = sig.split_at(7);
 
-        let mut hmac = HmacSha256::new_from_slice(sekrit.as_bytes()).unwrap();
-        hmac.update(payload.as_bytes());
-        if hmac.verify_slice(actual_signature.as_bytes()).is_err() {
+        log::trace!("Truncated sig: {}", actual_signature.clone());
+
+        //have to wrap it to stop timing attacks on comparison
+        let actual_signature = CtOutput::new(actual_signature.as_bytes());
+
+        let mut computed_signature = HmacSha256::new_from_slice(sekrit.as_bytes())
+            .unwrap()
+            .update(payload.as_bytes())
+            .finalize();
+
+        log::trace!("Computed sig: {}", computed_signature.clone());
+
+        if !actual_signature.ct_eq(computed_signature) {
             return Err(actix_web::error::ErrorBadRequest(
                 "Signature does not match!",
             ));
