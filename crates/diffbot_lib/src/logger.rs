9d1fd0975d94459b8c4df1c17a17105a509586c7
Katherine Kiefer <sterlite2902@gmail.com>
2023-08-17 20:54:48 +1000

    tracing changes

--- crates/diffbot_lib/src/logger.rs
+++ crates/diffbot_lib/src/logger.rs
@@ -15,8 +15,6 @@ pub fn init_logger(log_level: &str) -> eyre::Result<()> {
     tracing_subscriber::fmt()
         .with_level(true)
         .with_line_number(true)
-        .with_file(true)
-        .without_time()
         .with_max_level(get_log_level(log_level))
         .init();
     Ok(())
