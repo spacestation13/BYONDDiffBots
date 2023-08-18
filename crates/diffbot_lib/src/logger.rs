30082fc67822d2314e5d37c788a6f05628796752
Katherine Kiefer <sterlite2902@gmail.com>
2023-08-18 15:01:12 +1000

    fix it

--- crates/diffbot_lib/src/logger.rs
+++ crates/diffbot_lib/src/logger.rs
@@ -17,21 +17,21 @@ pub fn init_logger(
     log_level: &str,
     grafana_layer: Option<tracing_loki::Layer>,
 ) -> eyre::Result<()> {
-    // let builder = tracing_subscriber::fmt()
-    //     .with_level(true)
-    //     .with_line_number(true)
-    //     .with_max_level(get_log_level(log_level));
-    let stdout_layer = tracing_subscriber::fmt::layer()
-        .with_level(true)
-        .with_line_number(true)
-        .with_writer(std::io::stdout.with_max_level(get_log_level(log_level)));
     if let Some(layer) = grafana_layer {
         tracing_subscriber::registry()
             .with(layer)
-            .with(stdout_layer)
+            .with(
+                tracing_subscriber::fmt::layer()
+                    .with_level(true)
+                    .with_line_number(true)
+                    .with_writer(std::io::stdout.with_max_level(get_log_level(log_level))),
+            )
             .init();
     } else {
-        tracing_subscriber::registry().with(stdout_layer).init();
+        tracing_subscriber::fmt()
+            .with_level(true)
+            .with_line_number(true)
+            .init();
     }
 
     std::panic::set_hook(Box::new(tracing_panic::panic_hook));
